6280c719b87a43a84bcff3b2b706eb0305d63fbe
Shweta Sahoo <ssresponse1996@gmail.com>
2026-07-03 16:16:00 +1000

    Enable addSwitch function

--- R/GeneratePop.R
+++ R/GeneratePop.R
@@ -179,15 +179,13 @@ PopSelect = function(map,
   ## Sire
   RecPointListS = RecombinationPoint(map, SireSelect, recL, nChr, recProb)
   RecSirelist = RecPointListS$recList
-  #SireSelectFinal <- addSwitch(SireSelect, RecSirelist, minLength)
-  SireSelectFinal <- SireSelect
+  SireSelectFinal <- addSwitch(SireSelect, RecSirelist, minLength)
   rownames(SireSelectFinal) = rownames(SireSelect)
 
   ##Dam
   RecPointListD = RecombinationPoint(map, DamSelect, recL, nChr, recProb)
   RecDamlist = RecPointListD$recList
-  #DamSelectFinal <- addSwitch(DamSelect, RecDamlist, minLength)
-  DamSelectFinal <- DamSelect
+  DamSelectFinal <- addSwitch(DamSelect, RecDamlist, minLength)
   rownames(DamSelectFinal) = rownames(DamSelect)
 
   ###### Selection of random gametes of sire and dam
